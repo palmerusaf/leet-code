679a78c1ad8c1a61fa62319b704408c1f45a5347
Branden Palmer <brandenpalmer08@gmail.com>
2024-06-26 19:05:48 +0700

    add reorder-list.js

---
+++ reorder-list.js
@@ -0,0 +1,6 @@
+/**
+ * @typedef {{val:number|0,next:ListNode|null}} ListNode
+ * @param {ListNode} head
+ * @return {void} Do not return anything, modify head in-place instead.
+ */
+var reorderList = function(head) { };
