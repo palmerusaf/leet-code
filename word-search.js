1ab8fcfb6c0ef639e21da00b21cc74bc57d29ab9
Branden Palmer <brandenpalmer08@gmail.com>
2024-06-30 21:40:57 +0700

    improve word search

--- word-search.js
+++ word-search.js
@@ -3,7 +3,8 @@
  * @param {string} word
  * @return {boolean}
  */
-var exist = function(board, word) {
+var exist = function (board, word) {
+  if (!hasChars(board, word)) return false;
   for (let r = 0; r < board.length; r++) {
     for (let c = 0; c < board[r].length; c++) {
       if (dfs(r, c, 0)) {
@@ -25,6 +26,22 @@ var exist = function(board, word) {
       dfs(r, c - 1, i + 1);
     board[r][c] = word[i];
     return res;
+  }
+  function hasChars(board, word) {
+    let wc = {};
+    for (let i = 0; i < board.length; i++) {
+      for (let j = 0; j < board[i].length; j++) {
+        wc[board[i][j]] = (wc[board[i][j]] || 0) + 1;
+      }
+    }
+    for (let i = 0; i < word.length; i++) {
+      if (wc[word[i]] !== undefined && wc[word[i]] >= 0) {
+        wc[word[i]]--;
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 };
 
