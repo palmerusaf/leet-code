7e12e9529b0e2d8c2c412433c3b8070e0a09e448
Branden Palmer <brandenpalmer08@gmail.com>
2024-07-05 13:32:31 +0700

    solve word-search-ii

--- word-search-ii.js
+++ word-search-ii.js
@@ -1,13 +1,62 @@
 /**
+ * @typedef {{children:{[string]:PNode},wordIndex:number|-1}} PNode
+ */
+/**
  * @param {character[][]} board
  * @param {string[]} words
  * @return {string[]}
  */
 var findWords = function (board, words) {
-  return [];
+  // build prefix tree
+  /** @type PNode */
+  const root = { wordIndex: -1, children: {} };
+  for (let i = 0; i < words.length; i++) {
+    let iter = root;
+    for (let c = 0; c < words[i].length; c++) {
+      const char = words[i][c];
+      if (iter.children[char] === undefined) {
+        iter.children[char] = { children: {}, wordIndex: -1 };
+      }
+      iter = iter.children[char];
+    }
+    iter.wordIndex = i;
+  }
+  const res = new Set();
+  /**
+   * @param {character[][]} board
+   * @param {PNode} iter
+   */
+  const dfs = (r, c, iter) => {
+    if (iter === undefined) return;
+    if (iter.wordIndex > -1) res.add(words[iter.wordIndex]);
+    if (!board[r] || !board[r][c]) return;
+    const char = board[r][c];
+    board[r][c] = "*";
+    dfs(r + 1, c, iter.children[char]);
+    dfs(r - 1, c, iter.children[char]);
+    dfs(r, c + 1, iter.children[char]);
+    dfs(r, c - 1, iter.children[char]);
+    board[r][c] = char;
+  };
+  for (let r = 0; r < board.length; r++) {
+    for (let c = 0; c < board[r].length; c++) {
+      dfs(r, c, root);
+    }
+  }
+  return [...res.values()];
 };
 
 [
+  {
+    board: [["a", "b"]],
+    words: ["ba"],
+    exp: ["ba"],
+  },
+  {
+    board: [["a"]],
+    words: ["a"],
+    exp: ["a"],
+  },
   {
     board: [
       ["o", "a", "a", "n"],
@@ -22,9 +71,11 @@ var findWords = function (board, words) {
 
 function runTest({ board, words, exp }, index) {
   index++;
-  if (findWords(board, words).sort().toString() === exp.sort().toString()) {
+  const res = findWords(board, words);
+  if (res.sort().toString() === exp.sort().toString()) {
     console.log("Test", index, "Passed");
+  } else {
+    console.log("Test", index, "Failed");
+    console.log({ board, words, exp, res });
   }
-  console.log("Test", index, "Failed");
-  console.log({ board, words, exp });
 }
