1b1cc44733d3c2efc2bae1fffe105ce84e825d65
Branden Palmer <brandenpalmer08@gmail.com>
2024-07-31 12:47:42 +0700

    update test for class deff

--- number-of-connected-components-in-an-undirected-graph.js
+++ number-of-connected-components-in-an-undirected-graph.js
@@ -4,31 +4,7 @@ class Solution {
    * @param {number[][]} edges
    * @returns {number}
    */
-  countComponents(n, edges) {
-    if (!edges.length) return n;
-    const vis = {};
-    let count = 0;
-    const mp = {};
-    for (const [n1, n2] of edges) {
-      mp[n1] ? mp[n1].push(n2) : (mp[n1] = [n2]);
-    }
-    for (const [n2, n1] of edges) {
-      mp[n1] ? mp[n1].push(n2) : (mp[n1] = [n2]);
-    }
-    for (const n in mp) {
-      if (!vis[n]) count++;
-      dfs(n, n);
-    }
-    function dfs(n, prevN) {
-      if (vis[n]) return;
-      vis[n] = true;
-      for (const ad of mp[n]) {
-        if (ad === prevN) continue;
-        dfs(ad, n);
-      }
-    }
-    return count;
-  }
+  countComponents(n, edges) { }
 }
 
 /** @typedef {typeof test1} test */
@@ -63,7 +39,7 @@ const tests = [
 function runTest({ edges, exp, n }, index) {
   index++;
   // const s = Date.now();
-  const res = countComponents(n, edges);
+  const res = new Solution().countComponents(n, edges);
   // const runTime = Date.now() - s;
   // if (runTime > 600) {
   //   console.log("Test", index, "Failed");
