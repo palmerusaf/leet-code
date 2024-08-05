0c6ee80b0ac15d59a0cc4dbeb14204d59cc45822
Branden Palmer <brandenpalmer08@gmail.com>
2024-08-05 21:03:39 +0700

    add long test

--- jump-game.js
+++ jump-game.js
@@ -1,10 +1,25 @@
 import { runTest } from "./runTest.js";
 
 [
+  ((n) => ({
+    nums: Array(n + 1)
+      .fill(0)
+      .map((v) => (v = n > 0 ? --n : n)),
+    exp: false,
+  }))(27),
   { nums: [1, 2, 3], exp: true },
   { nums: [2, 3, 1, 1, 4], exp: true },
   { nums: [3, 2, 1, 0, 4], exp: false },
-].forEach(({ nums, exp }, i) => runTest({ exp, res: canJump(nums), index: i }));
+  { nums: [3, 2, 1, 0, 4], exp: false },
+].forEach(({ nums, exp }, i) =>
+  runTest({
+    startTime: Date.now(),
+    timeLimit: 400,
+    exp,
+    res: canJump(nums),
+    index: i,
+  }),
+);
 
 /** @param {number[]} nums @return {boolean} */
 function canJump(nums) {
