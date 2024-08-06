d26e9c3aafa3bc28f8fa1d65c2d7d46822f8926d
Branden Palmer <brandenpalmer08@gmail.com>
2024-08-06 15:47:35 +0700

    solve meeting-schedule-ii

--- meeting-schedule-ii.js
+++ meeting-schedule-ii.js
@@ -2,23 +2,35 @@ class Solution {
   /**  @typedef {{start:number,end:number}} Interval */
   /** @param {Interval[]} intervals @returns {number} */
   minMeetingRooms(intervals) {
-    if (intervals.length === 0) return 1;
-    let count = 1;
-    intervals.sort((a, b) => a.start - b.start);
-    let prevEnd = intervals[0].end;
-    for (const { start, end } of intervals.slice(1)) {
-      if (prevEnd > start) {
+    const starts = intervals.map(({ start }) => start).sort((a, b) => a - b);
+    const ends = intervals.map(({ end }) => end).sort((a, b) => a - b);
+    let [count, res, s, e] = Array(4).fill(0);
+    while (s < intervals.length) {
+      if (starts[s] < ends[e]) {
         count++;
+        s++;
+      } else {
+        count--;
+        e++;
       }
-      prevEnd = end;
+      res = Math.max(res, count);
     }
-    return count;
+    return res;
   }
 }
 
 import { runTest } from "./runTest.js";
 
 [
+  {
+    intervals: [
+      { start: 1, end: 5 },
+      { start: 5, end: 10 },
+      { start: 10, end: 15 },
+      { start: 15, end: 20 },
+    ],
+    exp: 1,
+  },
   {
     intervals: [
       { start: 1, end: 5 },
